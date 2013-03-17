60309e6fee5c1649330cd9da49d201828b122354
David DeSandro <desandrocodes@gmail.com>
2013-03-16 23:20:46 -0400

    add syntax highlighting

--- tasks/page.js
+++ tasks/page.js
@@ -3,7 +3,19 @@
 **/
 
 
+var highlightjs = require('highlight.js');
 var marked = require('marked');
+
+// alias XML syntax highlighting as HTML
+highlightjs.LANGUAGES.html = highlightjs.LANGUAGES.xml;
+// alias js as javascript
+highlightjs.LANGUAGES.js = highlightjs.LANGUAGES.javascript;
+
+marked.setOptions({
+  highlight: function( code, lang ) {
+    return lang ? highlightjs.highlight( lang, code ).value : code;
+  }
+});
 
 module.exports = function( grunt ) {
 
@@ -12,7 +24,7 @@ module.exports = function( grunt ) {
     var page = grunt.file.read('assets/page.html');
     var content = page.replace( '{{{ content }}}', readmeHTML );
 
-    grunt.file.write( 'build/index.html', content )
+    grunt.file.write( 'build/index.html', content );
   });
 
 };
