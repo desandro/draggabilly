0eb88719909ad57710cb4b16295afdd44ea466d1
David DeSandro <desandrocodes@gmail.com>
2015-12-22 08:48:40 -0500

    pair down requestAnimationFrame logic
    + remove cancelAnimationFrame

--- draggabilly.js
+++ draggabilly.js
@@ -65,37 +65,21 @@ function isElement( obj ) {
 
 // https://gist.github.com/1866474
 
-var lastTime = 0;
-var prefixes = 'webkit moz ms o'.split(' ');
-// get unprefixed rAF and cAF, if present
-var requestAnimationFrame = window.requestAnimationFrame;
-var cancelAnimationFrame = window.cancelAnimationFrame;
-// loop through vendor prefixes and get prefixed rAF and cAF
-var prefix;
-for( var i = 0; i < prefixes.length; i++ ) {
-  if ( requestAnimationFrame && cancelAnimationFrame ) {
-    break;
-  }
-  prefix = prefixes[i];
-  requestAnimationFrame = requestAnimationFrame || window[ prefix + 'RequestAnimationFrame' ];
-  cancelAnimationFrame  = cancelAnimationFrame  || window[ prefix + 'CancelAnimationFrame' ] ||
-                            window[ prefix + 'CancelRequestAnimationFrame' ];
-}
+// get rAF and cAF, prefixed, if present
+var requestAnimationFrame = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
 
+var lastTime = 0;
 // fallback to setTimeout and clearTimeout if either request/cancel is not supported
-if ( !requestAnimationFrame || !cancelAnimationFrame )  {
+if ( !requestAnimationFrame )  {
   requestAnimationFrame = function( callback ) {
     var currTime = new Date().getTime();
     var timeToCall = Math.max( 0, 16 - ( currTime - lastTime ) );
-    var id = window.setTimeout( function() {
+    var id = setTimeout( function() {
       callback( currTime + timeToCall );
     }, timeToCall );
     lastTime = currTime + timeToCall;
     return id;
-  };
-
-  cancelAnimationFrame = function( id ) {
-    window.clearTimeout( id );
   };
 }
 
